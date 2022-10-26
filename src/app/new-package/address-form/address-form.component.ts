a4455994da0633b77c4e024c8a0c2fbec815f359
Patrycja Stępień <patrycja.stepien@capgemini.com>
2022-10-26 16:03:10 +0200

    control container

--- src/app/new-package/address-form/address-form.component.ts
+++ src/app/new-package/address-form/address-form.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { ControlContainer, FormGroup, NgForm } from '@angular/forms';
 import { AddressForm } from '../../model/new-package-types';
 
 @Component({
-  selector: 'app-address-form[addressForm]',
+  selector: 'app-address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
+  styleUrls: ['./address-form.component.scss'],
+  providers: [{ provide: ControlContainer, useExisting: NgForm }]
 })
 export class AddressFormComponent {
 
-  @Input() addressForm!: FormGroup<AddressForm>
+  constructor(private readonly controlContainer: ControlContainer) {
+  }
+
+  get addressForm(): FormGroup<AddressForm> {
+    return this.controlContainer.control as FormGroup<AddressForm>;
+  }
 
 }
--- src/app/new-package/contact-form/contact-form.component.ts
+++ src/app/new-package/contact-form/contact-form.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { ControlContainer, FormGroup, FormGroupDirective, NgForm } from '@angular/forms';
 import { ContactForm } from '../../model/new-package-types';
 
 @Component({
-  selector: 'app-contact-form[contactForm]',
+  selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  styleUrls: ['./contact-form.component.scss'],
+  providers: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
 })
 export class ContactFormComponent {
 
-  @Input() contactForm!: FormGroup<ContactForm>;
+  constructor(private readonly controlContainer: ControlContainer) {
+  }
+
+  get contactForm(): FormGroup<ContactForm> {
+    return this.controlContainer.control as FormGroup<ContactForm>;
+  }
 
 }
